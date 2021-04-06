f24cbf615200cce6d352283ebf5979bcd3ed0428
Kerriea-star <ojared511@gmail.com>
2021-04-06 10:05:58 +0300

    Errors and exceptions

--- Errors and Exceptions.py
+++ Errors and Exceptions.py
@@ -64,3 +64,56 @@ try:
     this_fails()
 except ZeroDivisionError as err:
     print('Handling run-time error:', err)
+# User-defined Exceptions
+class Error(Exception):
+    """Base class exceptions for this module."""
+    pass
+class inputError(Error):
+    """Exceptions raised for errors in the input.
+
+    Attributes:
+    expression--input expression in which the error occurred
+    message--explanation if the error
+    """
+
+    def__init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+    class TransitionError(Error):
+        """Raised when an operation attempts toa satae transition that is not
+        allowed.
+
+        Attributes:
+            previous -- state at beginning of transition
+            next -- attempted new state
+            message -- explanation of why the specific transition is not allowed
+            """
+    def__init__(self, previous, next, message):
+        self.previoius = previous
+        self.next = next
+        self.message = message
+    # Defining clean-up actions
+    try:
+        raise KeyboardInterrupt
+    finally:
+        print('Goodbye, world!')
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("Division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("Executing finally clause")
+    divide(2, 1)
+    divide(2, 0)
+    divide("2", "1")
+# Predefined clean-up actions
+for line in open("myfile.txt"):
+    print(line, end="")
+with open("myfile.txt") as f:
+    for line in f:
+        print(line, end="")
+
+
